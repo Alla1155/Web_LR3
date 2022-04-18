1c92756c113017b0a0bceed9ccfbca3a8d5c29e6
Alla1155 <alla.efimenko.01@mail.ru>
2022-04-18 14:36:19 +0700

    commit2

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,12 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
 module.exports = {
-    output: {
-        publicPath: '/'
-    },
-    devServer: {
-      historyApiFallback: true,
-    },
-}
+  entry: './src/index.js',
+  mode:'development',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  plugins: [new HtmlWebpackPlugin()]
+};
