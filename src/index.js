f450254b617b0c8bc12c12373aa3edf7ff0d84cf
Alla1155 <alla.efimenko.01@mail.ru>
2022-04-19 19:12:12 +0700

    commit3

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
-console.log('hello');
+import './style.css'
+import 'owl.carousel/dist/assets/owl.carousel.css';
+import 'owl.carousel';
+import 'jquery/dist/jquery.js';
+
+$(document).ready(function(){
+  $(".owl-carousel").owlCarousel({
+    items: 3,
+    margin: 1,
+    loop: true,
+    nav: true
+  });
+});
--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,49 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const webpack = require('webpack');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
   mode:'development',
+  module: {
+    rules: [
+        {
+          test: /\.css$/i,
+          use: ['style-loader', 'css-loader'],
+        },
+        {
+          test: /\.(jpg|png|svg|jpeg)$/,
+          loader:'file-loader',
+          options:{
+            name: '[name].[ext]'
+          },
+        },
+    ],
+  },
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js'
   },
-  plugins: [new HtmlWebpackPlugin()]
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      template: 'src/index.html',
+      filename:'index.html'
+    }),
+    new webpack.ProvidePlugin({
+      $: 'jquery',
+      jQuery: 'jquery',
+      'window.jQuery': 'jquery'
+    }),
+    new CopyWebpackPlugin({
+      patterns:[
+        { 
+          from: './src/images',
+          to: 'assets/images'
+        }
+      ]
+    })
+  ]
 };
